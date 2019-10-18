d0701e4ec8fdbeb6e153ea630fd8dc347b793061
Maddie <mmsmith@uw.edu>
2019-10-18 18:12:07 +0000

    add function to scatter an lin regress tas and extent

--- notebooks/siutils.py
+++ notebooks/siutils.py
@@ -17,3 +17,31 @@ def Arctic_SIextent(SICONC_IN,CELLAREA_IN,MEMBER_IN):
     #sum area where SI conc > 15%
     ts_Arctic_extent = cellarea_extent.sum(dim=['nlat','nlon'])
     return ts_Arctic_extent
+
+def scatter_tas_SIE_linreg(TAS_ARCTIC_IN,SIE_ARCTIC_IN,MONTHS_IN,PLOTFLAG):
+    import calendar
+    slopes_all = []
+    r_all = []
+    if PLOTFLAG == True:
+        fig = plt.figure()
+    for m,mi in enumerate(MONTHS_IN):
+        CESM_airtemp_mi = TAS_ARCTIC_IN[mi::12].values
+        CESM_extent_mi = extent_NH[mi::12].values
+        monthname = calendar.month_name[mi+1]
+
+        slope,intercept,r_value, p_value, std_err = stats.linregress(CESM_airtemp_mi,CESM_extent_mi/1e12)
+        slopes_all.append(slope)
+        r_all.append(r_value)
+        
+        if PLOTFLAG == True:
+            ax = fig.add_subplot(1,len(MONTHS_IN),m+1)
+            ax.scatter(CESM_airtemp_mi,CESM_extent_mi/1e12)
+            ax.plot(CESM_airtemp_mi, intercept + slope*CESM_airtemp_mi, 'r')
+            ax.set_title(monthname+', slope: %f  ' % (slope))
+            #print("slope: %f  " % (slope))
+            ax.set_xlabel('Temp (K)')
+            ax.set_ylabel('SIE (millions km$^2$)')
+
+    if PLOTFLAG == True:
+        plt.show()
+    return slopes_all, r_all
