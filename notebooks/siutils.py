c975d85cb5e1a2ece37be0852dda00d95c33ba78
Default user <rc8653@my.bristol.ac.uk>
2019-10-18 18:24:39 +0000

    Updated siutils

--- notebooks/siutils.py
+++ notebooks/siutils.py
@@ -11,13 +11,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def rename_dimensions(DICT_IN, DICT_OUT):
+    """Renames the dimensions of DICT_IN from whatever they were to 
+       'j' and 'i'.
+       inputs:  
+            DICT_IN = dictionary 
+            DICT_OUT = empty dictionary 
+       outputs: 
+            DICT_OUT = full dictionary with new dimension names
+    """
     if 'x' in DICT_IN.dims:
         DICT_OUT = DICT_IN.rename(dict(x='j',y='i'))
+    elif 'ni' in DICT_IN.dims:
+        DICT_OUT = DICT_IN.rename(dict(nj='j',ni='i'))
     else: 
         DICT_OUT = DICT_IN
     return DICT_OUT
 
 def find_overlap_models(VAR_LIST,EXP,TABLE_ID,MODEL_INTERSECT,COL):
+    """Finds models that contain all the variables in VAR_LIST
+       inputs: 
+            VAR_LIST = list of variable names (list of (str))
+            EXP = name of experiment (str)
+            TABLE_ID = list of table ids associated with variable names (list of (str))
+            MODEL_INTERSECT = 
+            COL = output object of intake.open_esm_datastore 
+       outputs: 
+           MODEL_INTERSECT = list of models with all variables in VAR_LIST
+    """
     for v,variable_id in enumerate(VAR_LIST):
         query = dict(experiment_id=EXP, table_id=TABLE_ID[v], 
                      variable_id=variable_id, grid_label='gn')  
@@ -31,6 +51,8 @@ def find_overlap_models(VAR_LIST,EXP,TABLE_ID,MODEL_INTERSECT,COL):
     return MODEL_INTERSECT
 
 def time_to_dt(VAR):
+    """Defines a datetime variable and replaces time with datetime array. 
+    """
     if len(VAR['time'].attrs) is 0:
         print('yes')
         return VAR['time']
@@ -39,18 +61,30 @@ def time_to_dt(VAR):
         ds = xr.Dataset({'time': ('time', VAR.time.values, attrs)})
         var = xr.decode_cf(ds).time
         return var
+    
+def arctic_mean(VAR_DICT,VAR,MIN_LAT):
+    ones_full = np.ones((VAR.isel(time=0,member_id=0).shape))
+    area_weight = np.cos(np.deg2rad(VAR_DICT.lat)).expand_dims('lon',axis=1)*ones_full
+
+    var_armn = (VAR*area_weight).sel(lat=slice(MIN_LAT,90)).sum(
+        dim=['lat','lon'])/(area_weight.sel(lat=slice(MIN_LAT,90)).sum(dim=['lat','lon']))
+    return var_armn
 
 def AverageArctic_airtemperature(TAS_IN,MEMBER_IN):
     #select latitudes greater than 70N
     tas_Arctic = TAS_IN.sel(lat=slice(70,90))
+    # Area weighting
+    ones_full = np.ones((tas_dict[m].tas.isel(time=0,member_id=0).shape))
+    area_weight = np.cos(np.deg2rad(tas_dict[m].lat)).expand_dims('lon',axis=1)*ones_full
     #average over lat and lon, assumes 
     ts_tas_ArcticAve = tas_Arctic[MEMBER_IN,:,:].mean(dim=['lat','lon'])
     return ts_tas_ArcticAve
 
 def Arctic_SIextent(SICONC_IN,CELLAREA_IN,MEMBER_IN):
+    SICONC_IN=SICONC_IN.isel(i=slice(int(len(SICONC_IN.i)/2),
+                                                               len(SICONC_IN.i)))
     #cell areas only where SI concentration greater than 15% - must be same shape/format!
     cellarea_extent = CELLAREA_IN[MEMBER_IN,:,:].where(SICONC_IN[MEMBER_IN,:,:,:]>.15)
     #sum area where SI conc > 15%
-    ts_Arctic_extent = cellarea_extent.sum(dim=['nlat','nlon'])
+    ts_Arctic_extent = cellarea_extent.sum(dim=['i','j'])
     return ts_Arctic_extent
-
