830aea8f94715822cafa5a4528540f21feb3e195
Katie Brennan <mary.k.brennan6@gmail.com>
2020-05-05 21:07:53 -0600

    working version without notebooks. Many models not processed because no area cell files

--- notebooks/analysis/extract_cmip6.py
+++ notebooks/analysis/extract_cmip6.py
@@ -2,18 +2,24 @@ import intake
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
+import warnings
 
-from load_utils import get_cmip6_catalogue,find_overlap_models, rename_dimensions
-
+from load_utils import get_cmip6_catalogue,find_overlap_models,rename_dimensions,rename_lat_lon
 
 # Select which datasets are required by populating var_interest with details
+# var_interest = {'siconc':{'table_id':'SImon','grid':'gn'},
+#                 'tas':{'table_id':'Amon','grid':'gn'},
+#                 'areacello':{'table_id':'Ofx','grid':'gn'}}
 var_interest = {'siconc':{'table_id':'SImon','grid':'gn'},
-                'tas':{'table_id':'Amon','grid':'gn'},
-                'areacello':{'table_id':'Ofx','grid':'gn'}}
+                'tas':{'table_id':'Amon','grid':'gn'}}
 
 # Select name for this clollection of inputs
-collection_name = 'historical'
+#collection_name = 'historical'
 #collection_name = 'piControl'
+collection_name = 'ssp370'
+
+# IF True: will check to make sure only r*i1p1n1 experiments are included: 
+check_member_id = False
 
 #------------------------------------------------------------------
 dset_name, exp, var, table_id, grid_label = [], [], [], [], []
@@ -32,6 +38,7 @@ cmip6_collection = get_cmip6_catalogue()
 models_intersect = find_overlap_models(dset_name, exp, var, table_id, grid_label, cmip6_collection)
 print(collection_name+' model runs containing '+str([i for i in var_interest.keys()])+' = '+str(models_intersect))
 
+warnings.filterwarnings('ignore')
 # Get dictionary of file names for speficied data
 dset_dict = {}
 for i in range(0, len(dset_name)):
@@ -43,17 +50,22 @@ for i in range(0, len(dset_name)):
 for d in dset_dict.keys():
     print(d)
     dset_dict[d] = dset_dict[d].to_dataset_dict(zarr_kwargs={'consolidated': True, 'decode_times': True}, 
-                                           cdf_kwargs={'chunks': {'time': 1}, 'decode_times': True})    
+                                                cdf_kwargs={'chunks': {}, 'decode_times': True})    
 # Having some problems, times don't want to be decoded for picontrol. Might not be a problem but should investigate.
 
 # Rename dimensions to i,j so they're consistent across variables
 dset_dict_temp = {}
+dset_dict_temp_lalo = {}
 for d in dset_dict.keys():
     dset_dict_temp[d] = {}
+    dset_dict_temp_lalo[d] = {}
     for m in dset_dict[d].keys():
         dset_dict_temp[d][m] = rename_dimensions(dset_dict[d][m], dset_dict_temp)
-
-dset_dict = dset_dict_temp
+        if d == 'siconc':
+            dset_dict_temp_lalo[d][m] = rename_lat_lon(dset_dict_temp[d][m], dset_dict_temp_lalo)
+        else: 
+            dset_dict_temp_lalo[d][m] = dset_dict_temp[d][m]
+dset_dict = dset_dict_temp_lalo
 
 # Making key of dataset model name
 dset_dict_temp = {}
@@ -63,7 +75,6 @@ for d in dset_dict.keys():
         model = item.attrs['source_id']
         if model in models_intersect:
             dset_dict_temp[d][model] = item
-
 dset_dict = dset_dict_temp
 
 if 'areacello' in dset_dict.keys():
@@ -81,31 +92,37 @@ if 'areacella' in dset_dict.keys():
                 dset_dict[d][key]['areacella'] = dset_dict['areacella'][key]['areacella']    
                 
 # Ensure only ensemble members that overlap all data sets are included
-dset_dict_temp = {}
-
-for d in dset_dict.keys():
-    dset_dict_temp[d] = {}
-    
-for m in models_intersect:
-    ems = [0]
+if check_member_id == True: 
+    dset_dict_temp = {}
     for d in dset_dict.keys():
-        dset_dict_temp[d][m] = {}
-        if d is not 'areacello':
-            if ems[0]==0:
-                ems = dset_dict[d][m]['member_id'].values
-            else:
-                ems = list(set(ems) & set(dset_dict[d][m]['member_id'].values))
-    
-    for d in dset_dict.keys():                       
-        dset_dict_temp[d][m] = dset_dict[d][m].sel(member_id=ems)
-        
-    print(m, ems)
-    print()
-        
-dset_dict = dset_dict_temp
+        dset_dict_temp[d] = {}
+
+    for m in models_intersect:
+        ems = [0]
+        for d in dset_dict.keys():
+            dset_dict_temp[d][m] = {}
+            if d is not 'areacello':
+                if ems[0]==0:
+                    ems = dset_dict[d][m]['member_id'].values
+                else:
+                    ems = list(set(ems) & set(dset_dict[d][m]['member_id'].values))
+
+        for d in dset_dict.keys():                       
+            dset_dict_temp[d][m] = dset_dict[d][m].sel(member_id=ems)
+
+        print(m, ems)
+        print()
+
+    dset_dict = dset_dict_temp
+
+# add variable names to filename: 
+var_text = [v+'_' for v in list(var_interest.keys())]
+end_text=''
+for v in var_text:
+    end_text = end_text+v
 
 # Save dictionaries for future use
 save_flag = True
 if save_flag:
     if dset_dict:
-        np.save('dset_dict_' + collection_name + '.npy', dset_dict)
+        np.save('dset_dict_' + collection_name + '_' + end_text + 'extracted.npy', dset_dict)
--- notebooks/analysis/preprocess_cmip6.py
+++ notebooks/analysis/preprocess_cmip6.py
@@ -5,23 +5,75 @@ import xarray as xr
 from scipy import stats
 import warnings
 
-from analysis_utils import arctic_mean, Arctic_SIextent
+import analysis_utils as au
 
 # Load dataset
 #collection_fname = 'dset_dict_historical.npy'
-collection_fname = 'dset_dict_piControl.npy'
+#collection_fname = 'dset_dict_piControl.npy'
+#collection_fname = 'dset_dict_ssp370.npy'
+#collection_fname = 'dset_dict_historical_siconc_tas_extracted.npy'
+collection_fname = 'dset_dict_ssp370_siconc_tas_extracted.npy'
 # ------------------------------------------------------------------------
 
 dset_dict = np.load(collection_fname, allow_pickle='TRUE').item()
 first_dset = list(dset_dict.keys())[0]
 models_intersect = dset_dict[first_dset].keys()
 
+# Change latitude and longitude to coordinates (from variables)
+# dset_dict_temp = {}
+# for v in list(dset_dict.keys()):
+#     print('working on '+v)
+#     dset_dict_mods = {}
+#     for m in models_intersect:
+#         if 'latitude' in dset_dict[v][m].data_vars:
+# #            print('changing lat/lon')
+#             dset_dict_mods[m] = dset_dict[v][m].set_coords(['longitude', 'latitude'])
+#         else: 
+#             dset_dict_mods[m] = dset_dict[v][m]
+#     dset_dict_temp[v] = dset_dict_mods
+# dset_dict = dset_dict_temp
+
 si_dict = dset_dict['siconc']
 tas_dict = dset_dict['tas']
 
+# for m in models_intersect:
+#     si_dict[m]['sie_tot_arc'] = Arctic_SIextent(si_dict[m]['siconc'], 
+#                                                   si_dict[m]['areacello'], 15)
+#     tas_dict[m]['tas_arc_mean'] = arctic_mean(tas_dict[m], tas_dict[m]['tas'], 70)
+warnings.filterwarnings('ignore')
+
 for m in models_intersect:
-    si_dict[m]['sie_tot_arc'] = Arctic_SIextent(si_dict[m]['siconc'], si_dict[m]['areacello'], 15)
-    tas_dict[m]['tas_arc_mean'] = arctic_mean(tas_dict[m], tas_dict[m]['tas'], 70)
+    print('Calculating indices on model '+ m)
+    if 'areacello' in si_dict[m].keys():
+        areacello = si_dict[m]['areacello'] 
+    else: 
+        try:
+            areacello = au.get_cellareao(m)
+        except KeyError as e:
+            print('Skipping model '+m+' because no cell area file - KeyError: "%s"' % str(e))
+            continue    
+
+#    si_dict[m]['sie_tot_arc'] = au.Arctic_SIextent(si_dict[m]['siconc'], areacello, 15)
+    si_dict[m]['siextent'] = au.calc_siextent(si_dict[m]['siconc'],15)
+    si_dict[m]['sie_tot_arc'] = au.calc_tot_nh_siextent(si_dict[m]['siextent'],areacello,m)
+    tas_dict[m]['tas_arc_mean'] = au.calc_arctic_mean(tas_dict[m], tas_dict[m]['tas'], 70)
+    
+    # Calculate climatology, anomalies and stds for Arctic sea ice extent and mean Arctic temperature
+    # There are some performance warnings from dask. Hopefully not an issue?
+
+    # n.b. total anomalies don't add up to exactly zero for sie_tot_arc_anom
+    # this is just a precision thing because xarrays are only 8 sig figs
+
+    print('Calculating anomalies for indices')
+    var1 = dset_dict['siconc'][m]['sie_tot_arc']
+    dset_dict['siconc'][m]['sie_tot_arc_clim'] = var1.groupby('time.month').mean(dim='time')
+    dset_dict['siconc'][m]['sie_tot_arc_anom'] = var1.groupby('time.month') - dset_dict['siconc'][m]['sie_tot_arc_clim']
+    
+    var2 = dset_dict['tas'][m]['tas_arc_mean']
+    dset_dict['tas'][m]['tas_arc_mean_clim'] = var2.groupby('time.month').mean(dim='time')
+    dset_dict['tas'][m]['tas_arc_mean_anom'] = var2.groupby('time.month') -  dset_dict['tas'][m]['tas_arc_mean_clim'] 
+    
+warnings.filterwarnings('default')
     
 dset_dict['siconc'] = si_dict
 dset_dict['tas'] = tas_dict
@@ -32,6 +84,7 @@ dset_dict['tas'] = tas_dict
 warnings.filterwarnings('ignore')
 
 for d in dset_dict.keys():
+    print('Calculating climo for '+ d)
     if d not in ['areacello', 'areacella']:
         for m in models_intersect:
             dset_dict[d][m][d + '_clim'] = dset_dict[d][m][d].groupby('time.month').mean(dim='time')
@@ -49,27 +102,8 @@ warnings.filterwarnings('default')
 # Actually more likely to want to filter or remove moving average probably.
 # ------------------------------------------------------------------------
 
-# Calculate climatology, anomalies and stds for Arctic sea ice extent and mean Arctic temperature
-# There are some performance warnings from dask. Hopefully not an issue?
-warnings.filterwarnings('ignore')
-
-# n.b. total anomalies don't add up to exactly zero for sie_tot_arc_anom
-# this is just a precision thing because xarrays are only 8 sig figs
-
-for m in models_intersect:
-    var1 = dset_dict['siconc'][m]['sie_tot_arc']
-    dset_dict['siconc'][m]['sie_tot_arc_clim'] = var1.groupby('time.month').mean(dim='time')
-    dset_dict['siconc'][m]['sie_tot_arc_anom'] = var1.groupby('time.month') - dset_dict['siconc'][m]['sie_tot_arc_clim']
-    
-    var2 = dset_dict['tas'][m]['tas_arc_mean']
-    dset_dict['tas'][m]['tas_arc_mean_clim'] = var2.groupby('time.month').mean(dim='time')
-    dset_dict['tas'][m]['tas_arc_mean_anom'] = var2.groupby('time.month') -  dset_dict['tas'][m]['tas_arc_mean_clim'] 
-    
-warnings.filterwarnings('default')
-# ------------------------------------------------------------------------
-
 # Save dictionaries for future use
 save_flag = True
 if save_flag:
     if dset_dict:
-        np.save(collection_fname, dset_dict)
+        np.save(collection_fname[:-13]+'preprocessed.npy', dset_dict)
